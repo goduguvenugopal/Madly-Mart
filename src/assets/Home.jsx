a82498c00497d26e26c17be3b62d55078268be13
goduguvenugopal <149813252+goduguvenugopal@users.noreply.github.com>
2025-08-20 19:09:23 +0530

    add

--- src/assets/Home.jsx
+++ src/assets/Home.jsx
@@ -44,12 +44,12 @@ const Home = () => {
       <OffersPopUp />
       {/* categories section  */}
       <Carousel />
-      <div className="p-3 mt-5 w-full">
+      <div className="p-3 mt-5 pb-5 w-full">
         <h5 className="text-center text-2xl font-semibold">
           Product Categories
         </h5>
         <hr className="my-2 border border-orange-500" />
-        <div className="mt-4 py-6 grid  grid-cols-2 gap-y-5 gap-x-5 md:gap-y-6 lg:gap-y-6  md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+        <div className="mt-4 py-6 grid grid-cols-3 gap-y-5 gap-x-5 md:gap-y-7 lg:gap-y-9 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 xl:gap-x-8">
           {categories?.length > 0 ? (
             <>
               {categories?.map((item) => (
@@ -60,28 +60,28 @@ const Home = () => {
                       : `/products_by_category/${item.productCategoryName}`
                   }
                   key={item._id}
-                  className="group w-full h-full  md:w-52   lg:w-72   transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105 "
+                  className="group flex flex-col  w-[6.1rem]   h-fit  md:w-52   lg:w-[14rem]   transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105 "
                 >
-                  <div className=" relative">
+                  <div className="relative w-full h-full  md:w-full   lg:w-full ">
                     <LazyLoadImage
-                      src={item?.productImage.image}
+                      src={item?.productImage?.image}
                       alt={item?.productCategoryName}
-                      className="min-h-[200px] w-full rounded-lg"
+                      className="min-h-20 w-full rounded-lg"
                       effect="opacity"
                       placeholderSrc="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyMDAiIGhlaWdodD0iMjAwIj4KICA8cmVjdCB3aWR0aD0iMTAwJSIgaGVpZ2h0PSIxMDAlIiBmaWxsPSIjZWVlIi8+PC9zdmc+"
                     />
 
                     {item.available === "no" && (
-                      <div className="absolute flex items-center justify-center  top-0 left-0 w-full h-full bg-black bg-opacity-0  text-white ">
-                        <span className="bg-black text-white bg-opacity-55 rounded p-1 text-lg lg:text-2xl">
+                      <div className="absolute flex items-center justify-center text-center top-0 left-0 w-full h-full bg-black bg-opacity-0  text-white ">
+                        <span className="bg-black text-white bg-opacity-55 text-[0.8rem] rounded p-1 md:text-lg lg:text-2xl">
                           Available Soon
                         </span>
                       </div>
                     )}
                   </div>
 
-                  <div className="mt-2 text-center">
-                    <h3 className="text-lg capitalize font-medium text-gray-800">
+                  <div className="mt-2 text-center ">
+                    <h3 className="text-base  md:text-lg capitalize font-medium text-gray-800">
                       {item?.productCategoryName}
                     </h3>
                   </div>
